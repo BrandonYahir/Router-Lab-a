ee76697b861125e82be94438bc3965de3512e11d
BrandonYahir <byahir722@gmail.com>
2021-10-11 23:38:19 -0500

    Se agrega el Proyecto

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,12 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <Titulo texto="Pagina de Inicio" />
-    <p>Brandon Yahir Flores Vazquez <br>
-       Grupo: 3 A <br>
-       Materia: Aplicaciones Web Orientadas a Servicios <br>
-       Carrera: Tecnologias de la Informacion Desarrollo de Software Multiplataforma
-       </p>
   </div>
+<h3>Brandon Yahir Flores Vazquez <br>
+    Grupo: 3 A <br>
+    Materia: Aplicaciones Web Orientadas a Servicios <br>
+    Carrera: Tecnologias de la Informacion Desarrollo de Software Multiplataforma
+    </h3>
 </template>
 
 <script>
@@ -22,7 +22,7 @@ export default {
 </script>
 
 <style>
-p {
+h3 {
 background-color: rgb(5, 253, 5);
 
 margin: auto;
